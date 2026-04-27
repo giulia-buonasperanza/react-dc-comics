2491ee959f0441a77fcd9dfaafff7f8176e2add5
giulia-buonasperanza <giulia.9402@gmail.com>
2026-04-27 19:28:24 +0200

    stile banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,16 @@
 function Banner() {
     return (
-        <ul>
-            <li><img src="/img/buy-comics-digital-comics.png" alt="Digital Comics"/><a href="/">DIGITAL COMICS</a></li>
-            <li><img src="/img/buy-comics-merchandise.png" alt="Merchandise"/><a href="/comics">DC MERCHANDISE</a></li>
-            <li><img src="/img/buy-comics-subscriptions.png" alt="Subscriptions"/><a href="/characters">SUBSCRITION</a></li>
-            <li><img src="/img/buy-comics-shop-locator.png" alt="Shop Locator"/><a href="/">COMIC SHOP LOCATOR</a></li>
-            <li><img src="/img/buy-dc-power-visa.png" alt="DC Power VISA"/><a href="/comics">DC POWER VISA</a></li>
-        </ul>
+        <div className="banner">
+            <div className="bannerContainer">
+                <ul className="BannerList">
+                    <li><img src="/img/buy-comics-digital-comics.png" alt="Digital Comics" /><a href="/">DIGITAL COMICS</a></li>
+                    <li><img src="/img/buy-comics-merchandise.png" alt="Merchandise" /><a href="/comics">DC MERCHANDISE</a></li>
+                    <li><img src="/img/buy-comics-subscriptions.png" alt="Subscriptions" /><a href="/characters">SUBSCRITION</a></li>
+                    <li><img src="/img/buy-comics-shop-locator.png" alt="Shop Locator" /><a href="/">COMIC SHOP LOCATOR</a></li>
+                    <li><img src="/img/buy-dc-power-visa.svg" alt="DC Power VISA" /><a href="/comics">DC POWER VISA</a></li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
